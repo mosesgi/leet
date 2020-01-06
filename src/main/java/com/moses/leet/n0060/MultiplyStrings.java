416ccfcce888a19d7beb9c59bc1dfa84f6f6a639
Moses Ji <mosionji@163.com>
2020-01-06 21:42:10 +0800

    43

--- src/main/java/com/moses/leet/n0060/MultiplyStrings.java
+++ src/main/java/com/moses/leet/n0060/MultiplyStrings.java
@@ -5,6 +5,9 @@ import java.math.BigInteger;
 public class MultiplyStrings {
 
     public String multiply(String num1, String num2){
+        if(num1.equals("0") || num2.equals("0")){
+            return "0";
+        }
         char[] char1 = num1.toCharArray();
         char[] char2 = num2.toCharArray();
         String rst = "0";
@@ -38,23 +41,43 @@ public class MultiplyStrings {
             rst = addBigNums(rst, sb.toString());
         }
 
-        return "";
+        return rst;
     }
 
     private String addBigNums(String num1, String num2){
         char[] char2 = num2.toCharArray();
         char[] char1 = num1.toCharArray();
-        int posDif = char1.length - char2.length;
+        int posDif = char2.length - char1.length;
+        int promote = 0;
+        StringBuilder sb = new StringBuilder();
         for(int i = char2.length; i>0; i--){
             int posSec = char2[i-1] - 48;
-
+            int posFirst = 0;
+            if(i-1 >= posDif) {
+                posFirst = char1[i - 1 - posDif] - 48;
+            }
+            int tmp = posSec + posFirst + promote;
+            String tmpStr = tmp+"";
+            if(tmpStr.length() == 2) {
+                promote = tmpStr.charAt(0) - 48;
+                sb.insert(0, tmpStr.charAt(1));
+            } else {
+                promote = 0;
+                sb.insert(0, tmpStr.charAt(0));
+            }
         }
-        return "";
+        if(promote != 0){
+            sb.insert(0, promote);
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
 
         String num1 = "2", num2 = "3";
+        System.out.println(new MultiplyStrings().multiply(num1, num2));
+
+        num1 = "9133"; num2 = "0";
         System.out.println(new MultiplyStrings().multiply(num1, num2));
 
         num1 = "123"; num2 = "456";
