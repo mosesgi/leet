00f90c856b0cd699008745824f7ffc1e9e40ef13
Moses Ji <mosionji@163.com>
2020-01-14 20:03:04 +0800

    76...

--- src/main/java/com/moses/leet/n0080/MinWindowSubstring.java
+++ src/main/java/com/moses/leet/n0080/MinWindowSubstring.java
@@ -8,6 +8,7 @@ import java.util.Set;
  * https://leetcode.com/problems/minimum-window-substring/
  */
 public class MinWindowSubstring {
+
     public String minWindow(String s, String t) {
         int sLen = s.length();
         int tLen = t.length();
@@ -78,7 +79,7 @@ public class MinWindowSubstring {
     private boolean isMatching(Map<Character, Integer> map){
         Set<Character> set = map.keySet();
         for(Character c: set){
-            if(map.get(c) == 0){
+            if(map.get(c) > 0){
                 return false;
             }
         }
@@ -90,6 +91,14 @@ public class MinWindowSubstring {
         String t = "ABC";
         System.out.println(new MinWindowSubstring().minWindow(s, t));
 
+        s = "aaaaaaaaaaaabbbbbcdd";
+        t = "abcdd";
+        System.out.println(new MinWindowSubstring().minWindow(s, t));
+
+        s = "bbaa";
+        t = "aba";
+        System.out.println(new MinWindowSubstring().minWindow(s, t));
+
         s = "CDEADOBECABEBANC";
         t = "ABC";
         System.out.println(new MinWindowSubstring().minWindow(s, t));
@@ -97,5 +106,82 @@ public class MinWindowSubstring {
         s = "CDEADOBECODEBANC";
         t = "ABC";
         System.out.println(new MinWindowSubstring().minWindow(s, t));
+
+        s = "abc";
+        t = "cba";
+        System.out.println(new MinWindowSubstring().minWindow(s, t));
     }
+
+
+    //time limit exceeded. This is not O(n)
+    public String minWindowInvalid(String s, String t){
+        int sLen = s.length();
+        int tLen = t.length();
+        char[] tChars = t.toCharArray();
+
+        if(sLen < tLen){
+            return "";
+        } else if(sLen == tLen){
+            Map<Character, Integer> map = new HashMap<>();
+            for(char c: tChars){
+                if(map.containsKey(c)){
+                    map.put(c, map.get(c) + 1);
+                } else {
+                    map.put(c, 1);
+                }
+            }
+            for(int i=0; i<sLen; i++){
+                char c = s.charAt(i);
+                if(map.containsKey(c)){
+                    map.put(c, map.get(c) - 1);
+                }
+            }
+            if(isMatching(map)){
+                return s;
+            }
+        }
+
+        int searchLen = tLen;
+
+        while(searchLen <= sLen){
+            Map<Character, Integer> map = new HashMap<>();
+            for(char c: tChars){
+                if(map.containsKey(c)){
+                    map.put(c, map.get(c) + 1);
+                } else {
+                    map.put(c, 1);
+                }
+            }
+            for(int i=0; i<=s.length()-searchLen; i++){
+//                System.out.println("i=" + i + ", " + s.substring(i, i+searchLen));
+                if(i==0){
+                    for(int j=i; j<searchLen; j++){
+                        char c = s.charAt(j);
+                        if(map.containsKey(c)){
+                            map.put(c, map.get(c) -1);
+                        }
+                    }
+                    if(isMatching(map)){
+                        return s.substring(0, searchLen);
+                    }
+                } else {
+                    char prevLeft = s.charAt(i-1);
+                    if(map.containsKey(prevLeft)){
+                        map.put(prevLeft, map.get(prevLeft) + 1);
+                    }
+                    char newRight = s.charAt(i+searchLen-1);
+                    if(map.containsKey(newRight)){
+                        map.put(newRight, map.get(newRight) - 1);
+                        if(isMatching(map)){
+                            return s.substring(i, i+searchLen);
+                        }
+                    }
+                }
+            }
+            searchLen++;
+        }
+        return "";
+    }
+
+
 }
