f89c70b845fd791f44bb0cd2f1d02697aa3a58b7
Moses Ji <mosionji@163.com>
2020-01-16 18:47:08 +0800

    91 complete

--- src/main/java/com/moses/leet/n0100/DecodeWays.java
+++ src/main/java/com/moses/leet/n0100/DecodeWays.java
@@ -9,9 +9,42 @@ import java.util.Map;
  * https://leetcode.com/problems/decode-ways/
  */
 public class DecodeWays {
+
+    //DP solution.
+    //For example, the String now is "123xxxx" and we know all the result from 2.
+    //Because 12<26, we can make this string either"12"+"3xxxx" or 1+23xxxx which is exactly memo[n]=memo[n+1]+memo[n+2].
+    public int numDecodings(String s){
+        if(s.length()==0 || s.startsWith("0")){
+            return 0;
+        }
+        if(s.length() == 1){
+            return 1;
+        }
+
+        int len = s.length();
+        int[] cache = new int[len+1];
+        cache[len] = 1;
+        //initialize len-1
+        cache[len-1] = s.charAt(len-1) == '0'?0:1;
+        for(int i=len-2; i>=0; i--){
+            if(s.charAt(i) == '0'){
+                cache[i] = 0;
+                continue;
+            }
+            int tmpInt = Integer.parseInt(s.substring(i, i+2));
+            if(tmpInt <= 26){
+                cache[i] = cache[i+1] + cache[i+2];
+            } else{
+                cache[i] = cache[i+1];
+            }
+        }
+        return cache[0];
+    }
+
+
+    //my solution: divide and conquer +++ backtrack. similar efficiency with DP :) hahahahahahahaha!!!
     int count = 0;
-    boolean isZero = false;
-    public int numDecodings(String s) {
+    public int numDecodingsMine(String s) {
         if(s.length() == 0 || s.startsWith("0")){
             return 0;
         }
@@ -19,9 +52,31 @@ public class DecodeWays {
             return 1;
         }
 
-        backtrack(s, 0);
+        return divide(s);
+    }
 
-        return count;
+    private int divide(String s){
+        if(s.length()>10){
+            int mid = s.length()/2;
+            if(s.charAt(mid) < '3'){
+                while(s.charAt(mid) < '3' && mid<s.length()-1){
+                    mid++;
+                }
+            }
+            if(mid+1 == s.length()){
+                count=0;
+                backtrack(s, 0);
+                return count;
+            }
+            int int1 = divide(s.substring(0, mid+1));
+            int int2 = divide(s.substring(mid+1));
+            return int1*int2;
+        } else {
+            count=0;
+            backtrack(s, 0);
+            return count;
+        }
+
     }
 
     private void backtrack(String s, int index) {
@@ -65,5 +120,7 @@ public class DecodeWays {
         System.out.println(new DecodeWays().numDecodings("022"));      //0
 
         System.out.println(new DecodeWays().numDecodings("1787897759966261825913315262377298132516969578441236833255596967132573482281598412163216914566534565"));
+
+        System.out.println(new DecodeWays().numDecodings("8435098221275814436356964965276761914618252012127634945951156398377952112951644342731746644843122125"));
     }
 }
