32214f83a1b207e159784f6a1e0c1895f4768a23
Moses Ji <mosionji@163.com>
2020-05-25 23:15:48 +0800

    ms: 15 - 437

---
+++ src/main/java/com/moses/leet/n0900/SuperEggDrop.java
@@ -0,0 +1,12 @@
+package com.moses.leet.n0900;
+
+public class SuperEggDrop {
+    //k=2,n=6.   3; 4; 5
+    // k=3,n=14.  7; 11; 12; 13
+    //k=2,n=18.  9; +7  = 8
+    //k=4,n=18.  9; 14; 16; 17;  =4
+    //wrong
+    public int superEggDrop(int K, int N) {
+        return 0;
+    }
+}
